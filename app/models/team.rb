3abe5eacef0a8f85a353a32f77e72a0b16adfb01
Dariusz Bliszczyk <dariusz.bliszczyk@gmail.com>
2014-12-25 12:49:02 +0100

    Add show team simple view.

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,6 @@ class Team < ActiveRecord::Base
   has_many :players
   has_many :memberships
 
-  has_attached_file :logo, :styles => { :icon => "32x32>" }, :default_url => "/images/missing_:style.png"
+  has_attached_file :logo, styles: { icon: "32x32>", medium: "300x300>" }, default_url: "/images/missing_:style.png"
   validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
 end
